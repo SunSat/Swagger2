48cdbdd18383bff0fc77e293e102d6c87db200cd
sunsat <sundar.sat84@gmail.com>
2022-02-05 21:00:48 +0530

    initial commit

---
+++ src/main/java/com/example/swagger2/controller/BookController.java
@@ -0,0 +1,37 @@
+package com.example.swagger2.controller;
+
+import com.example.swagger2.model.Book;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.swagger2.api.*;
+import org.springframework.web.context.request.NativeWebRequest;
+
+@RestController
+public class BookController implements BookApi {
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return BookApi.super.getRequest();
+    }
+
+    @Override
+    public ResponseEntity<String> addBook(Book book) {
+        return BookApi.super.addBook(book);
+    }
+
+    @Override
+    public ResponseEntity<List<Book>> getBooks() {
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book()
+                .title("Name of the Wind")
+                .author("Patrick"));
+        bookList.add(new Book()
+                .title("Mistborn")
+                .author("Brandon"));
+        return ResponseEntity.ok(bookList);
+    }
+}
